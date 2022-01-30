c122f4f601f4136f35b952dad1dd3504bbb63225
Otávio Pace <otaviopp8@gmail.com>
2022-01-30 12:04:58 -0300

    04: temp fill, doesn't look at keyboard, only makes the whole screen black

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,45 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// [x] make a part of the screen black
+// [x] make write black loop
+// [x] stop at the final address
+// [x] graceful stop hehe
+// [ ] make a part of the screen black if keyboard pressed
+// [ ] make the whole screen blink based of keyboard input
+
+// setup:
+@KBD
+D=A
+// save in LOOP address the value of KBD,
+// aka 1 address after the end of the screen
+@LOOP
+M=D
+
+// point to the beginning of the screen memory map
+@SCREEN
+// @24573 // for debugging
+D=A
+
+// loop:
+(LOOP)
+
+// point to the next memory map address
+// set in D in the end of the last loop
+// iteration
+A=D
+
+// black line
+M=-1
+
+// go to next screen memory map address
+D=D+1
+
+// stop at the end of the screen memory map
+@LOOP
+M-D;JNE
+
+// infinite loop
+// end of program
+@END
+0;JMP
