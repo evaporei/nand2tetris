8c11013d524e2e5db270ad83eef76a328f7985de
Eva Pace <eba.pachi@gmail.com>
2022-07-29 18:46:50 -0300

    06: main parser structure

--- projects/06/assembler/src/main.rs
+++ projects/06/assembler/src/main.rs
@@ -1,3 +1,12 @@
+use assembler::parser::Parser;
+use std::{env, fs};
+
+/// The assembler converts the Hack computer machine language
+/// from the text form, to the binary form.
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    let file_name = &args[0];
+    let program = fs::read_to_string(file_name).expect("hack program doesn't exist");
+
+    let _tokens = Parser::parse(program.lines());
 }
---
+++ projects/06/assembler/src/parser.rs
@@ -0,0 +1,10 @@
+use crate::token::Token;
+use std::str::Lines;
+
+pub struct Parser {}
+
+impl Parser {
+    pub fn parse(_program_lines: Lines) -> Vec<Token> {
+        vec![]
+    }
+}
