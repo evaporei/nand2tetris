f11e2bdccdcdbb25c0e799683eaf0d4de9a2046a
Eva Pace <eba.pachi@gmail.com>
2022-07-29 23:19:58 -0300

    06: format comp C-instruction field

--- projects/06/assembler/src/instruction.rs
+++ projects/06/assembler/src/instruction.rs
@@ -37,12 +37,50 @@ impl Instruction {
 
 #[derive(Debug, PartialEq)]
 pub struct CInstruction {
-    #[allow(dead_code)]
     dest: Option<Symbol>,
-    #[allow(dead_code)]
     comp: Symbol,
-    #[allow(dead_code)]
     jump: Option<Symbol>,
+}
+
+impl CInstruction {
+    fn comp(c: &str) -> u16 {
+        match &c[..] {
+            // a = 0
+            "0" => "0101010",
+            "1" => "0111111",
+            "-1" => "0111010",
+            "D" => "0001100",
+            "A" => "0110000",
+            "!D" => "0001101",
+            "!A" => "0110001",
+            "-D" => "0001111",
+            "-A" => "0110011",
+            "D+1" => "0011111",
+            "A+1" => "0110111",
+            "D-1" => "0001110",
+            "A-1" => "0110010",
+            "D+A" => "0000010",
+            "D-A" => "0010011",
+            "A-D" => "0000111",
+            "D&A" => "0000000",
+            "D|A" => "0010101",
+            // a = 1
+            "M" => "1110000",
+            "!M" => "1110001",
+            "-M" => "1110011",
+            "M+1" => "1110111",
+            "M-1" => "1110010",
+            "D+M" => "1000010",
+            "D-M" => "1010011",
+            "M-D" => "1000111",
+            "D&M" => "1000000",
+            "D|M" => "1010101",
+            _ => panic!("non existent comp conversion"),
+        }
+        .to_string()
+        .parse()
+        .unwrap()
+    }
 }
 
 use std::fmt;
@@ -51,7 +89,14 @@ impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::A(address) => write!(f, "0{:015b}", address.parse::<u16>().unwrap()),
-            Self::C(_c_instr) => write!(f, "111"),
+            Self::C(CInstruction {
+                dest: _dest,
+                comp,
+                jump: _jump,
+            }) => {
+                write!(f, "111")?;
+                write!(f, "{:06b}", CInstruction::comp(&comp))
+            }
         }
     }
 }
