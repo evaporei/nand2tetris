0aa1dd2fecaea381028af6ce41b6bee35b7ed558
Eva Pace <eba.pachi@gmail.com>
2023-02-08 16:18:03 -0300

    07: push constant codegen

--- projects/07/vm/src/instr.rs
+++ projects/07/vm/src/instr.rs
@@ -9,20 +9,21 @@ pub enum Segment {
     Const,
 }
 
-impl fmt::Display for Segment {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let s = match self {
-            Self::Const => "const",
-        };
-
-        write!(f, "{}", s)
-    }
-}
-
 impl fmt::Display for Instr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let s = match self {
-            Self::Push(segment, i) => format!("push {segment} {i}"),
+            Self::Push(segment, i) => match segment {
+                Segment::Const => format!(
+                    "\
+@{i}
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1"
+                ),
+            },
             Self::Add => "add".into(),
         };
 
