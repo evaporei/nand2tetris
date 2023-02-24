311e5d0817c178e07a74bcb0ec2de18e29e8b263
Eva Pace <eba.pachi@gmail.com>
2023-02-24 14:51:31 -0300

    07: remove LOGICAL_ prefix in labels

--- projects/07/vm/src/instr.rs
+++ projects/07/vm/src/instr.rs
@@ -94,23 +94,23 @@ D=M
 M=M-1
 A=M
 D=D-M
-@LOGICAL_GT_ELSE_{i}
+@GT_ELSE_{i}
 D;JGE
 
 // (IF-then) it's greater than or equal to zero
 @SP
 A=M
 M=0
-@LOGICAL_GT_END_{i}
+@GT_END_{i}
 0;JMP
 
 // (IF-else) -> no greater than found
-(LOGICAL_GT_ELSE_{i})
+(GT_ELSE_{i})
 @SP
 A=M
 M=-1
 
-(LOGICAL_GT_END_{i})
+(GT_END_{i})
 @SP
 M=M+1"
             ),
@@ -124,23 +124,23 @@ D=M
 M=M-1
 A=M
 D=D-M
-@LOGICAL_LT_ELSE_{i}
+@LT_ELSE_{i}
 D;JLE
 
 // (IF-then) it's less than or equal to zero
 @SP
 A=M
 M=0
-@LOGICAL_LT_END_{i}
+@LT_END_{i}
 0;JMP
 
 // (IF-else) -> no less than found
-(LOGICAL_LT_ELSE_{i})
+(LT_ELSE_{i})
 @SP
 A=M
 M=-1
 
-(LOGICAL_LT_END_{i})
+(LT_END_{i})
 @SP
 M=M+1"
             ),
