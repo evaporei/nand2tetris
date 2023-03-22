e0bdb71190b4f154cedca9f8c407de85b5efaa78
Eva Pace <eba.pachi@gmail.com>
2023-03-22 15:45:49 -0300

    08: finish abstraction for file handling

--- projects/07/vm/src/main.rs
+++ projects/07/vm/src/main.rs
@@ -3,6 +3,7 @@ use std::ffi::OsStr;
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use vm::instr::Instr;
 use vm::parser::Parser;
 
 /// Removes extension from file name.
@@ -11,6 +12,28 @@ fn file_name(file: &str) -> Option<String> {
         .file_name()
         .and_then(OsStr::to_str)
         .map(|name| name.chars().take_while(|ch| *ch != '.').collect())
+}
+
+fn compile(source: &str) -> Vec<Instr> {
+    Parser::parse(source.lines())
+}
+
+fn create_asm_file(file: &str) -> File {
+    let mut new_file = PathBuf::from(file);
+
+    new_file.set_extension("asm");
+
+    File::create(new_file).expect("failed to create assembly file")
+}
+
+fn write_asm(mut file: File, file_name: &str, instructions: Vec<Instr>) {
+    for instruction in instructions {
+        file.write_all(instruction.to_assembly(&file_name).as_bytes())
+            .expect("failed to write instruction to translated file");
+
+        file.write_all(b"\n")
+            .expect("failed to write new line to translated file");
+    }
 }
 
 /// The vm converts the vm byte code into Hack Assembly
@@ -22,30 +45,16 @@ fn main() {
     if metadata.is_file() {
         let file = file_or_dir;
 
-        let program = fs::read_to_string(file.clone()).expect("vm program doesn't exist");
+        let source = fs::read_to_string(file).expect("vm program doesn't exist");
 
-        let instructions = Parser::parse(program.lines());
+        let instructions = compile(&source);
 
-        let mut new_file = PathBuf::from(file);
+        let new_file = create_asm_file(file);
 
-        new_file.set_extension("asm");
-
-        let mut translated_file = File::create(new_file).expect("failed to create assembly file");
-
-        let file_name = file_name(file).unwrap();
-
-        for instruction in instructions {
-            translated_file
-                .write_all(instruction.to_assembly(&file_name).as_bytes())
-                .expect("failed to write instruction to translated file");
-
-            translated_file
-                .write_all(b"\n")
-                .expect("failed to write new line to translated file");
-        }
+        write_asm(new_file, &file_name(file).unwrap(), instructions);
     } else if metadata.is_dir() {
         // TODO: handle dir of vm files
     } else {
-        panic!("unsupported parameter, passing symlink maybe?");
+        panic!("unsupported parameter, maybe passing symlink?");
     }
 }
