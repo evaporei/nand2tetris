99c872784857a8602649293fdcb816423e77586a
Eva Pace <eba.pachi@gmail.com>
2023-05-04 16:51:34 -0300

    10: boilerplate code for file management in compiler

--- projects/10/compiler/src/main.rs
+++ projects/10/compiler/src/main.rs
@@ -1,3 +1,51 @@
+use compiler::tokenizer::Tokenizer;
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
+
+fn create_tokens_file(file: &str) -> File {
+    let mut new_file = PathBuf::from(file);
+
+    let name_with_t = format!("{}T", new_file.file_stem().unwrap().to_str().unwrap());
+
+    new_file.set_file_name(name_with_t);
+    new_file.set_extension("xml");
+
+    File::create(new_file).expect("failed to create assembly file")
+}
+
+fn write_tokens(file: &mut File, tokens: Vec<String>) {
+    for token in tokens {
+        file.write_all(token.as_bytes())
+            .expect("failed to write token to tokens file");
+
+        file.write_all(b"\n")
+            .expect("failed to write new line to tokens file");
+    }
+}
+
 fn main() {
-    println!("double compilerrrr!!!!");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let file_or_dir = &args[0];
+    let metadata = fs::metadata(file_or_dir).unwrap();
+
+    if metadata.is_file() {
+        let file = file_or_dir;
+
+        let source = fs::read_to_string(file).expect("vm program doesn't exist");
+
+        let tokens = Tokenizer::tokenize(source.lines());
+
+        let mut new_file = create_tokens_file(file);
+
+        write_tokens(&mut new_file, tokens);
+    } else if metadata.is_dir() {
+        panic!("directories not supported yet");
+    } else {
+        panic!("unsupported parameter, maybe passing symlink?");
+    }
 }
---
+++ projects/10/compiler/src/tokenizer.rs
@@ -0,0 +1,9 @@
+use std::str::Lines;
+
+pub struct Tokenizer;
+
+impl Tokenizer {
+    pub fn tokenize(_lines: Lines) -> Vec<String> {
+        vec![]
+    }
+}
